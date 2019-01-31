f239a66e5dd52f4f0149a307789909c5ffc2b704
Michael Mokry <michael.mokry@att.com>
2019-01-31 13:16:55 -0600

    PDPX Healthcheck/Statistic RESTful API entry point
    Includes:
    - Basic code structure modeled after policy distribution
    - Code implementation to support Healthcheck/Statistics RESTful API entry point
    - JUnits
    - Fixed Checkstyles issues and added some missing statistics classes and
    Junits
    - Made changes per Jim's comments
    - Made more changes per Jim's comments
    	> made gson field static
    	> using AssertThatThrownBy() mechanic from AssertJ
    	> added setup and teardown to correctly terminate activator in
    	Junit
    - Made corrections to the statitics endpoint and junits
    
    Change-Id: Iad40272beceff8a0f99966440e96a84fc2043b12
    Issue-ID: POLICY-1436
    Signed-off-by: Michael Mokry <michael.mokry@att.com>

---
+++ main/src/main/java/org/onap/policy/pdpx/main/rest/StatisticsReport.java
@@ -0,0 +1,166 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdpx.main.rest;
+
+/**
+ * Class to represent statistics report of xacmlPdp service.
+ *
+ */
+public class StatisticsReport {
+
+    private int code;
+    private long totalPoliciesCount;
+    private long permitDecisionsCount;
+    private long denyDecisionsCount;
+    private long indeterminantDecisionsCount;
+    private long notApplicableDecisionsCount;
+
+
+    /**
+     * Returns the code of this {@link StatisticsReport} instance.
+     *
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * Set code in this {@link StatisticsReport} instance.
+     *
+     * @param code the code to set
+     */
+    public void setCode(final int code) {
+        this.code = code;
+    }
+
+    /**
+     * Returns the totalPoliciesCount of this {@link StatisticsReport} instance.
+     *
+     * @return the totalPoliciesCount
+     */
+    public long getTotalPoliciesCount() {
+        return totalPoliciesCount;
+    }
+
+    /**
+     * Set totalPoliciesCount in this {@link StatisticsReport} instance.
+     *
+     * @param totalPoliciesCount the totalPoliciesCount to set
+     */
+    public void setTotalPoliciesCount(long totalPoliciesCount) {
+        this.totalPoliciesCount = totalPoliciesCount;
+    }
+
+    /**
+     * Returns the permitDecisionsCount of this {@link StatisticsReport} instance.
+     *
+     * @return the permitDecisionsCount
+     */
+    public long getPermitDecisionsCount() {
+        return permitDecisionsCount;
+    }
+
+    /**
+     * Set permitDecisionsCount in this {@link StatisticsReport} instance.
+     *
+     * @param permitDecisionsCount the permitDecisionsCount to set
+     */
+    public void setPermitDecisionsCount(long permitDecisionsCount) {
+        this.permitDecisionsCount = permitDecisionsCount;
+    }
+
+    /**
+     * Returns the denyDecisionsCount of this {@link StatisticsReport} instance.
+     *
+     * @return the denyDecisionsCount
+     */
+    public long getDenyDecisionsCount() {
+        return denyDecisionsCount;
+    }
+
+    /**
+     * Set denyDecisionsCount in this {@link StatisticsReport} instance.
+     *
+     * @param denyDecisionsCount the denyDecisionsCount to set
+     */
+    public void setDenyDecisionsCount(long denyDecisionsCount) {
+        this.denyDecisionsCount = denyDecisionsCount;
+    }
+
+    /**
+     * Returns the indeterminantDecisionsCount of this {@link StatisticsReport} instance.
+     *
+     * @return the indeterminantDecisionsCount
+     */
+    public long getIndeterminantDecisionsCount() {
+        return indeterminantDecisionsCount;
+    }
+
+    /**
+     * Set indeterminantDecisionsCount in this {@link StatisticsReport} instance.
+     *
+     * @param indeterminantDecisionsCount the indeterminantDecisionsCount to set
+     */
+    public void setIndeterminantDecisionsCount(long indeterminantDecisionsCount) {
+        this.indeterminantDecisionsCount = indeterminantDecisionsCount;
+    }
+
+    /**
+     * Returns the notApplicableDecisionsCount of this {@link StatisticsReport} instance.
+     *
+     * @return the notApplicableDecisionsCount
+     */
+    public long getNotApplicableDecisionsCount() {
+        return notApplicableDecisionsCount;
+    }
+
+    /**
+     * Set notApplicableDecisionsCount in this {@link StatisticsReport} instance.
+     *
+     * @param notApplicableDecisionsCount the notApplicableDecisionsCount to set
+     */
+    public void setNotApplicableDecisionsCount(long notApplicableDecisionsCount) {
+        this.notApplicableDecisionsCount = notApplicableDecisionsCount;
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("StatisticsReport [code=");
+        builder.append(getCode());
+        builder.append(", totalPoliciesCount=");
+        builder.append(getTotalPoliciesCount());
+        builder.append(", permitDecisionsCount=");
+        builder.append(getPermitDecisionsCount());
+        builder.append(", denyDecisionsCount=");
+        builder.append(getDenyDecisionsCount());
+        builder.append(", indeterminantDecisionsCount=");
+        builder.append(getIndeterminantDecisionsCount());
+        builder.append(", notApplicableDecisionsCount=");
+        builder.append(getNotApplicableDecisionsCount());
+        builder.append("]");
+        return builder.toString();
+    }
+}
---
+++ main/src/main/java/org/onap/policy/pdpx/main/rest/XacmlPdpStatisticsManager.java
@@ -0,0 +1,139 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdpx.main.rest;
+
+/**
+ * Class to hold statistical data for xacmlPdp component.
+ *
+ */
+public class XacmlPdpStatisticsManager {
+
+    private static long totalPoliciesCount;
+    private static long permitDecisionsCount;
+    private static long denyDecisionsCount;
+    private static long indeterminantDecisionsCount;
+    private static long notApplicableDecisionsCount;
+
+    private XacmlPdpStatisticsManager() {
+        throw new IllegalStateException("Instantiation of the class is not allowed");
+    }
+
+    /**
+     * Method to update the xacml pdp total policies count.
+     *
+     * @return the total
+     */
+    public static long updateTotalPoliciesCount() {
+        return ++totalPoliciesCount;
+    }
+
+    /**
+     * Method to update the number of permit decisions.
+     *
+     * @return the permitDecisionsCount
+     */
+    public static long updatePermitDecisionsCount() {
+        return ++permitDecisionsCount;
+    }
+
+    /**
+     * Method to update the number of deny decisions.
+     *
+     * @return the denyDecisionsCount
+     */
+    public static long updateDenyDecisionsCount() {
+        return ++denyDecisionsCount;
+    }
+
+    /**
+     * Method to update the number of indeterminant decisions.
+     *
+     * @return the indeterminantDecisionsCount
+     */
+    public static long updateIndeterminantDecisionsCount() {
+        return ++indeterminantDecisionsCount;
+    }
+
+    /**
+     * Method to update the number of not applicable decisions.
+     *
+     * @return the notApplicableDecisionsCount
+     */
+    public static long updateNotApplicableDecisionsCount() {
+        return ++notApplicableDecisionsCount;
+    }
+
+    /**
+     * Returns the current value of totalPoliciesCount.
+
+     * @return the totalPoliciesCount
+     */
+    public static long getTotalPoliciesCount() {
+        return totalPoliciesCount;
+    }
+
+    /**
+     * Returns the current value of permitDecisionsCount.
+
+     * @return the permitDecisionsCount
+     */
+    public static long getPermitDecisionsCount() {
+        return permitDecisionsCount;
+    }
+
+    /**
+     * Returns the current value of denyDecisionsCount.
+
+     * @return the denyDecisionsCount
+     */
+    public static long getDenyDecisionsCount() {
+        return denyDecisionsCount;
+    }
+
+    /**
+     * Returns the current value of indeterminantDecisionsCount.
+
+     * @return the indeterminantDecisionsCount
+     */
+    public static long getIndeterminantDecisionsCount() {
+        return indeterminantDecisionsCount;
+    }
+
+    /**
+     * Returns the current value of notApplicableDecisionsCount.
+
+     * @return the notApplicableDecisionsCount
+     */
+    public static long getNotApplicableDecisionsCount() {
+        return notApplicableDecisionsCount;
+    }
+
+    /**
+     * Reset all the statistics counts to 0.
+     */
+    public static void resetAllStatistics() {
+        totalPoliciesCount = 0L;
+        permitDecisionsCount = 0L;
+        denyDecisionsCount = 0L;
+        indeterminantDecisionsCount = 0L;
+        notApplicableDecisionsCount = 0L;
+    }
+}
