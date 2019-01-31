5f9325ef4d6344d89a3431f8badb668cc02bb9f7
Michael Mokry <michael.mokry@att.com>
2019-01-31 13:16:55 -0600

    PDPX Healthcheck/Statistic RESTful API entry point
    Includes:
    - Basic code structure modeled after policy distribution
    - Code implementation to support Healthcheck/Statistics RESTful API entry point
    - JUnits
    
    Change-Id: Iad40272beceff8a0f99966440e96a84fc2043b12
    Issue-ID: POLICY-1436
    Signed-off-by: Michael Mokry <michael.mokry@att.com>

---
+++ main/src/main/java/org/onap/policy/pdpx/main/parameters/XacmlPdpParameterHandler.java
@@ -0,0 +1,82 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdpx.main.parameters;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+import org.onap.policy.pdpx.main.PolicyXacmlPdpException;
+import org.onap.policy.pdpx.main.startstop.XacmlPdpCommandLineArguments;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+import org.onap.policy.common.parameters.GroupValidationResult;
+
+/**
+ * This class handles reading, parsing and validating of policy xacml pdp parameters from JSON files.
+ */
+public class XacmlPdpParameterHandler {
+    private static final Logger LOGGER = FlexLogger.getLogger(XacmlPdpParameterHandler.class);
+
+    /**
+     * Read the parameters from the parameter file.
+     *
+     * @param arguments the arguments passed to policy xacml pdp
+     * @return the parameters read from the configuration file
+     * @throws PolicyXacmlPdpException on parameter exceptions
+     */
+    public XacmlPdpParameterGroup getParameters(final XacmlPdpCommandLineArguments arguments)
+            throws PolicyXacmlPdpException {
+        XacmlPdpParameterGroup xacmlPdpParameterGroup = null;
+
+        // Read the parameters
+        try {
+            // Read the parameters from JSON using Gson
+            final Gson gson = new GsonBuilder().create();
+            xacmlPdpParameterGroup = gson.fromJson(new FileReader(arguments.getFullConfigurationFilePath()),
+                    XacmlPdpParameterGroup.class);
+        } catch (final Exception e) {
+            final String errorMessage = "error reading parameters from \"" + arguments.getConfigurationFilePath()
+                    + "\"\n" + "(" + e.getClass().getSimpleName() + "):" + e.getMessage();
+            LOGGER.error(errorMessage, e);
+            throw new PolicyXacmlPdpException(errorMessage, e);
+        }
+
+        // The JSON processing returns null if there is an empty file
+        if (xacmlPdpParameterGroup == null) {
+            final String errorMessage = "no parameters found in \"" + arguments.getConfigurationFilePath() + "\"";
+            LOGGER.error(errorMessage);
+            throw new PolicyXacmlPdpException(errorMessage);
+        }
+
+        // validate the parameters
+        final GroupValidationResult validationResult = xacmlPdpParameterGroup.validate();
+        if (!validationResult.isValid()) {
+            String returnMessage =
+                    "validation error(s) on parameters from \"" + arguments.getConfigurationFilePath() + "\"\n";
+            returnMessage += validationResult.getResult();
+
+            LOGGER.error(returnMessage);
+            throw new PolicyXacmlPdpException(returnMessage);
+        }
+
+        return xacmlPdpParameterGroup;
+    }
+}
---
+++ main/src/test/java/org/onap/policy/pdpx/main/rest/TestXacmlPdpRestServer.java
@@ -0,0 +1,117 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdpx.main.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.junit.Test;
+import org.onap.policy.pdpx.main.PolicyXacmlPdpException;
+import org.onap.policy.pdpx.main.parameters.CommonTestData;
+import org.onap.policy.pdpx.main.parameters.RestServerParameters;
+import org.onap.policy.pdpx.main.startstop.Main;
+import org.onap.policy.common.endpoints.report.HealthCheckReport;
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+
+/**
+ * Class to perform unit test of HealthCheckMonitor.
+ *
+ */
+public class TestXacmlPdpRestServer {
+
+    private static final Logger LOGGER = FlexLogger.getLogger(TestXacmlPdpRestServer.class);
+    private static final String NOT_ALIVE = "not alive";
+    private static final String ALIVE = "alive";
+    private static final String SELF = "self";
+    private static final String NAME = "Policy Xacml PDP";
+
+    @Test
+    public void testHealthCheckSuccess() throws PolicyXacmlPdpException, InterruptedException {
+        final String reportString = "Report [name=Policy Xacml PDP, url=self, healthy=true, code=200, message=alive]";
+        final Main main = startXacmlPdpService();
+        final HealthCheckReport report = performHealthCheck();
+        validateReport(NAME, SELF, true, 200, ALIVE, reportString, report);
+        stopXacmlPdpService(main);
+    }
+
+    @Test
+    public void testHealthCheckFailure() throws InterruptedException {
+        final String reportString = "Report [name=Policy Xacml PDP, url=self, healthy=false, code=500, message=not alive]";
+        final RestServerParameters restServerParams = new CommonTestData().getRestServerParameters(false);
+        restServerParams.setName(CommonTestData.PDPX_GROUP_NAME);
+        final XacmlPdpRestServer restServer = new XacmlPdpRestServer(restServerParams);
+        restServer.start();
+        final HealthCheckReport report = performHealthCheck();
+        validateReport(NAME, SELF, false, 500, NOT_ALIVE, reportString, report);
+        assertTrue(restServer.isAlive());
+        assertTrue(restServer.toString().startsWith("XacmlPdpRestServer [servers="));
+        restServer.shutdown();
+    }
+
+    private Main startXacmlPdpService() {
+        final String[] xacmlPdpConfigParameters = { "-c", "parameters/XacmlPdpConfigParameters.json" };
+        return new Main(xacmlPdpConfigParameters);
+    }
+
+    private void stopXacmlPdpService(final Main main) throws PolicyXacmlPdpException {
+        main.shutdown();
+    }
+
+    private HealthCheckReport performHealthCheck() throws InterruptedException {
+        HealthCheckReport response = null;
+        final ClientConfig clientConfig = new ClientConfig();
+
+        final HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic("healthcheck", "zb!XztG34");
+        clientConfig.register(feature);
+
+        final Client client = ClientBuilder.newClient(clientConfig);
+        final WebTarget webTarget = client.target("http://localhost:6969/healthcheck");
+
+        final Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        while (response == null) {
+            try {
+                response = invocationBuilder.get(HealthCheckReport.class);
+            } catch (final Exception exp) {
+                LOGGER.info("the server is not started yet. We will retry again");
+            }
+        }
+        return response;
+    }
+
+    private void validateReport(final String name, final String url, final boolean healthy, final int code,
+            final String message, final String reportString, final HealthCheckReport report) {
+        assertEquals(name, report.getName());
+        assertEquals(url, report.getUrl());
+        assertEquals(healthy, report.isHealthy());
+        assertEquals(code, report.getCode());
+        assertEquals(message, report.getMessage());
+        assertEquals(reportString, report.toString());
+    }
+}
