2085fd202be89ee270bfe0d72bc99fbc051addea
Pamela Dragosh <pdragosh@research.att.com>
2019-03-03 19:01:08 -0500

    Foundation for Monitoring Decisions
    This sets the foundation for building the XACML PDP
    engine that supports Decisions for onap.Monitoring
    policy types.
    
    Mostly stubbed code for now, will most likely get
    re-arranged.
    
    Put in the cucumber basics, but no implementation yet.
    
    Need policy/parent to be merged before this will pass.
    
    Issue-ID: POLICY-1273
    Change-Id: Ifb455065316fd3ec0a025b5833b334ee72f65adc
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ applications/src/test/java/cucumber/Stepdefs.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefs {
+
+    @Given("TCA Policy is loaded")
+    public void tca_Policy_is_loaded() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("A Decision Request is received")
+    public void a_Decision_Request_is_received() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should return TCA Policy as JSON")
+    public void i_should_return_TCA_Policy_as_JSON() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
