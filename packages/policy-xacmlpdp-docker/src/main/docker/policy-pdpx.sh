ccd40ce52be8fbf7ba032f2db189fc0c70861b7e
Michael Mokry <mm117s@att.com>
2019-06-03 15:13:03 -0500

    Create the operationshistory table from pdpx
    Added create table sql statement to the policy-pdpx.sh to create the operationshistory table during
    xacml-pdp installation
    
    Change-Id: Iaae63dc65db1ff9181de12928e072f04c87e50e7
    Issue-ID: POLICY-1827
    Signed-off-by: Michael Mokry <michael.mokry@att.com>

--- packages/policy-xacmlpdp-docker/src/main/docker/policy-pdpx.sh
+++ packages/policy-xacmlpdp-docker/src/main/docker/policy-pdpx.sh
@@ -52,6 +52,25 @@ if [[ -f ${POLICY_HOME}/etc/mounted/xacml.properties ]]; then
     cp -f "${POLICY_HOME}"/etc/mounted/xacml.properties  "${POLICY_HOME}"/apps/guard/
 fi
 
+mysql -upolicy_user -ppolicy_user -h policydb<<EOF
+use operationshistory;
+
+create table if not exists operationshistory (
+	id int(11) not null auto_increment,
+	closedLoopName varchar(255) not null,
+	requestId varchar(50),
+	actor varchar(50) not null,
+	operation varchar(50) not null,
+	target varchar(50) not null,
+	starttime timestamp not null,
+	outcome varchar(50) not null,
+	message varchar(255),
+	subrequestId varchar(50),
+	endtime timestamp not null default current_timestamp,
+	PRIMARY KEY (id)
+);
+EOF
+
 echo "Policy Xacml PDP config file: $CONFIG_FILE"
 echo "Policy Xacml PDP topic properties file: $PROP_FILE"
 
