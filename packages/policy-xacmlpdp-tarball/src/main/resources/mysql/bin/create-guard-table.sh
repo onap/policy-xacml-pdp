5187557600d09b5db11282d1a85b5d54dbcb081e
Michael Mokry <michael.mokry@att.com>
2019-07-23 13:54:30 -0500

    Fix guard table creation during startup issue
    - Added -hpolicydb to the mysql command in the script
    - Made this configurable by utilizing the existing parameters in
    xacml.properties for db connections.  No OOM or CSIT changes should be
    needed now.
    
    PATCH UPDATE:
    	- used Awk to extract property values
    	- removed echo except one needed for the base64 command, as far
    	as I can tell it is needed when assigning output to a variable,
    	I tried to get it to work unsuccessfully.
    	- added exit code 2 for missing properties as suggested by Jorge
    	and Jim
    
    PATCH UPDATE:
    	- remove echo from base64 using Jim's suggestion
    	- Added code to remove escape backslashes from properties file,
    	I didn't see these locally but when testing in windriver they
    	show up in the xacml.properties file on OOM installs
    
    Change-Id: I79c0502d8a4fadd2160785014fcb9db3c0faab5e
    Issue-ID: POLICY-1934
    Signed-off-by: Michael Mokry <michael.mokry@att.com>

--- packages/policy-xacmlpdp-tarball/src/main/resources/mysql/bin/create-guard-table.sh
+++ packages/policy-xacmlpdp-tarball/src/main/resources/mysql/bin/create-guard-table.sh
@@ -1,7 +1,5 @@
-#!/bin/bash -xv
+#!/bin/bash
 #
-# ============LICENSE_START=======================================================
-#  Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,4 +18,38 @@
 #
 SQL_FILE="${POLICY_HOME}/mysql/sql/createguardtable.sql"
 
-mysql -upolicy_user -ppolicy_user < "${SQL_FILE}"
+# Remove escape backslashes if present and save output in temp file
+sed 's/\\//g' "${POLICY_HOME}"/apps/guard/xacml.properties > temp.properties
+
+# Extract Maria DB Credential properties from xacml.properties file
+DB_HOSTNAME=$(awk -F[/:] '$1 == "javax.persistence.jdbc.url=jdbc" { print $3 $5 }' temp.properties)
+DB_USERNAME=$(awk -F= '$1 == "javax.persistence.jdbc.user" { print $2 }' temp.properties)
+DB_PASSWORD=$(awk -F= '$1 == "javax.persistence.jdbc.password" { print $2 }' temp.properties | base64 -d)
+
+# Remove temp file
+if [ -f temp.properties ]
+  then
+    rm temp.properties
+fi
+
+if [ -z "$DB_HOSTNAME" ]
+  then
+    echo "No Mariadb host provided in xacml.properties."
+    exit 2
+fi
+
+if [ -z "$DB_USERNAME" ]
+  then
+    echo "No Mariadb username provided in xacml.properties."
+    exit 2
+fi
+
+if [ -z "$DB_PASSWORD" ]
+  then
+    echo "No Mariadb password provided in xacml.properties."
+    exit 2
+fi
+
+# Execute mysql command using sql file to create table
+mysql -u${DB_USERNAME} -p${DB_PASSWORD} -h${DB_HOSTNAME} < "${SQL_FILE}"
+
