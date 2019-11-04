fe3a8ec467beae89bca9d10b8b5b39f98c81ca01
Pamela Dragosh <pdragosh@research.att.com>
2019-11-04 16:18:08 -0500

    Implement closest match algorithm
    Needed to add more obligations, which make it easier to
    scan through obligations and be able to pull information
    about the policy.
    
    Adding weight as an obligation for Optimization policies in
    order to implement "closest match" algorithm.
    
    Moved Obligation to a support class.
    
    Added JUnit code coverage on the translator classes.
    
    Split some methods up to reduce complexity via sonar.
    
    Issue-ID: POLICY-2066
    Change-Id: Ibb13d2dc0a63ab2a6d585b0697a0c1d129fa8f7b
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ applications/common/src/main/java/org/onap/policy/pdp/xacml/application/common/OnapObligation.java
@@ -0,0 +1,259 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common;
+
+import com.att.research.xacml.api.AttributeAssignment;
+import com.att.research.xacml.api.Identifier;
+import com.att.research.xacml.api.Obligation;
+import com.google.gson.Gson;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.AttributeAssignmentExpressionType;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.AttributeValueType;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.EffectType;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.ObjectFactory;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.ObligationExpressionType;
+
+@Getter
+@ToString
+public class OnapObligation {
+
+    @Getter(AccessLevel.NONE)
+    private static final ObjectFactory factory = new ObjectFactory();
+
+    @Getter(AccessLevel.NONE)
+    private static final Gson gson = new Gson();
+
+    private String policyId;
+    private String policyType;
+    private String policyContent;
+    private Integer weight;
+
+    /**
+     * Constructor from an obligation.
+     *
+     * @param obligation Obligation object
+     */
+    public OnapObligation(Obligation obligation) {
+        //
+        // Scan through the obligations for them
+        //
+        for (AttributeAssignment assignment : obligation.getAttributeAssignments()) {
+            scanAttribute(assignment);
+        }
+    }
+
+    /**
+     * Constructor for just the policy details.
+     *
+     * @param policyId String
+     * @param policyContent String
+     */
+    public OnapObligation(String policyId, String policyContent) {
+        this.policyId = policyId;
+        this.policyContent = policyContent;
+    }
+
+
+    /**
+     * Constructor for policy details, type and weight.
+     *
+     * @param policyId String
+     * @param policyContent String
+     * @param policyType String
+     * @param weight int
+     */
+    public OnapObligation(String policyId, String policyContent, String policyType, Integer weight) {
+        this.policyId = policyId;
+        this.policyContent = policyContent;
+        this.policyType = policyType;
+        this.weight = weight;
+    }
+
+    /**
+     * getPolicyContentAsMap returns the policy as a map for convience.
+     *
+     * @return {@code Map<String, Object>}
+     */
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> getPolicyContentAsMap() {
+        if (this.policyContent == null) {
+            return null;
+        }
+        return gson.fromJson(this.policyContent, Map.class);
+    }
+
+    /**
+     * Generates default obligation using default Permit and Obligation Id.
+     *
+     * @return ObligationExpressionType object
+     */
+    public ObligationExpressionType generateObligation() {
+        return this.generateObligation(EffectType.PERMIT, ToscaDictionary.ID_OBLIGATION_REST_BODY);
+    }
+
+    /**
+     * generateObligation - generates an obligation object with the attributes that exist. Note:
+     * any null values will result in NO attribute assignment for that attribute.
+     *
+     * @param effectType EffectType object
+     * @param obligationId Id for the obligation
+     * @return ObligationExpressionType object
+     */
+    public ObligationExpressionType generateObligation(EffectType effectType, Identifier obligationId) {
+        //
+        // Create an ObligationExpression
+        //
+        ObligationExpressionType obligation = new ObligationExpressionType();
+        obligation.setFulfillOn(effectType);
+        obligation.setObligationId(obligationId.stringValue());
+        //
+        // Update the obligation
+        //
+        updateObligation(obligation);
+        //
+        // Convenience return
+        //
+        return obligation;
+    }
+
+    /**
+     * Updates an existing Obligation object with the attributes that exist. Note: any null values
+     * will result in NO attribute assignment for that attribute.
+     *
+     * @param obligation ObligationExpressionType object
+     * @return ObligationExpressionType object
+     */
+    public ObligationExpressionType updateObligation(ObligationExpressionType obligation) {
+        //
+        // Add policy-id
+        //
+        addOptionalAttributeToObligation(obligation, ToscaDictionary.ID_OBLIGATION_POLICY_ID,
+                ToscaDictionary.ID_OBLIGATION_POLICY_ID_DATATYPE,
+                ToscaDictionary.ID_OBLIGATION_POLICY_ID_CATEGORY,
+                policyId);
+        //
+        // Add policy contents
+        //
+        addOptionalAttributeToObligation(obligation, ToscaDictionary.ID_OBLIGATION_POLICY_CONTENT,
+                ToscaDictionary.ID_OBLIGATION_POLICY_CONTENT_DATATYPE,
+                ToscaDictionary.ID_OBLIGATION_POLICY_CONTENT_CATEGORY,
+                policyContent);
+        //
+        // Add the weight
+        //
+        addOptionalAttributeToObligation(obligation, ToscaDictionary.ID_OBLIGATION_POLICY_WEIGHT,
+                ToscaDictionary.ID_OBLIGATION_POLICY_WEIGHT_DATATYPE,
+                ToscaDictionary.ID_OBLIGATION_POLICY_WEIGHT_CATEGORY,
+                weight);
+        //
+        // Add the policy type
+        //
+        addOptionalAttributeToObligation(obligation, ToscaDictionary.ID_OBLIGATION_POLICY_TYPE,
+                ToscaDictionary.ID_OBLIGATION_POLICY_TYPE_DATATYPE,
+                ToscaDictionary.ID_OBLIGATION_POLICY_TYPE_CATEGORY,
+                policyType);
+        //
+        // Return as a convenience
+        //
+        return obligation;
+    }
+
+    /**
+     * scanAttribute - scans the assignment for a supported obligation assignment. Applications
+     * can override this class and provide their own custom attribute assignments if desired.
+     *
+     * @param assignment AttributeAssignment object
+     * @return true if found an ONAP supported attribute
+     */
+    protected boolean scanAttribute(AttributeAssignment assignment) {
+        //
+        // Check for our supported attributes. Note: Cannot use a switch
+        // as Identifier isn't a constant.
+        //
+        if (ToscaDictionary.ID_OBLIGATION_POLICY_ID.equals(assignment.getAttributeId())) {
+            policyId = assignment.getAttributeValue().getValue().toString();
+            return true;
+        } else if (ToscaDictionary.ID_OBLIGATION_POLICY_TYPE.equals(assignment.getAttributeId())) {
+            policyType = assignment.getAttributeValue().getValue().toString();
+            return true;
+        } else if (ToscaDictionary.ID_OBLIGATION_POLICY_CONTENT.equals(assignment.getAttributeId())) {
+            policyContent = assignment.getAttributeValue().getValue().toString();
+            return true;
+        } else if (ToscaDictionary.ID_OBLIGATION_POLICY_WEIGHT.equals(assignment.getAttributeId())) {
+            weight = Integer.decode(assignment.getAttributeValue().getValue().toString());
+            return true;
+        }
+        //
+        // By returning true, we indicate this isn't an attribute
+        // supported in the this class. Targeted for applications
+        // that derive from this class in order to extend it.
+        //
+        return false;
+    }
+
+    /**
+     * Creates the necessary objects to insert into the obligation, if the value object is not null.
+     *
+     * @param obligation Incoming Obligation
+     * @param id Attribute Id
+     * @param datatype Attribute's Data type
+     * @param category Attributes Category
+     * @param theValue Attribute value
+     * @return obligation Incoming obligation
+     */
+    protected ObligationExpressionType addOptionalAttributeToObligation(ObligationExpressionType obligation,
+            Identifier id, Identifier datatype, Identifier category, Object theValue) {
+        //
+        // Simple check for null
+        //
+        if (theValue == null) {
+            return obligation;
+        }
+        //
+        // Create an AttributeValue for it
+        //
+        AttributeValueType value = new AttributeValueType();
+        value.setDataType(datatype.stringValue());
+        value.getContent().add(theValue.toString());
+        //
+        // Create our AttributeAssignmentExpression where we will
+        // store the contents of the policy id.
+        //
+        AttributeAssignmentExpressionType expressionType = new AttributeAssignmentExpressionType();
+        expressionType.setAttributeId(id.stringValue());
+        expressionType.setCategory(category.stringValue());
+        expressionType.setExpression(factory.createAttributeValue(value));
+        //
+        // Add it to the obligation
+        //
+        obligation.getAttributeAssignmentExpression().add(expressionType);
+        //
+        // Return as convenience
+        //
+        return obligation;
+    }
+
+}
---
+++ applications/common/src/test/java/org/onap/policy/pdp/xacml/application/common/TestUtilsCommon.java
@@ -0,0 +1,126 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common;
+
+import com.att.research.xacml.api.AttributeAssignment;
+import com.att.research.xacml.api.Decision;
+import com.att.research.xacml.api.IdReference;
+import com.att.research.xacml.api.Obligation;
+import com.att.research.xacml.api.Response;
+import com.att.research.xacml.api.StatusCode;
+import com.att.research.xacml.api.XACML3;
+import com.att.research.xacml.std.IdentifierImpl;
+import com.att.research.xacml.std.StdAttributeAssignment;
+import com.att.research.xacml.std.StdAttributeValue;
+import com.att.research.xacml.std.StdIdReference;
+import com.att.research.xacml.std.StdMutableObligation;
+import com.att.research.xacml.std.StdMutableResponse;
+import com.att.research.xacml.std.StdMutableResult;
+import com.att.research.xacml.std.StdStatus;
+import com.att.research.xacml.std.StdVersion;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class TestUtilsCommon {
+
+    /**
+     * createAttributeAssignment.
+     *
+     * @param <T> Object can be String, Integer, Double, Boolean
+     * @param id String attribute id
+     * @param category String for the attribute category
+     * @param value Object containing a value
+     * @return AttributeAssignment object
+     */
+    public static <T> AttributeAssignment createAttributeAssignment(String id, String category, T value) {
+        StdAttributeValue<T> attributeValue = null;
+        if (value instanceof String) {
+            attributeValue = new StdAttributeValue<>(XACML3.ID_DATATYPE_STRING, value);
+        } else if (value instanceof Integer) {
+            attributeValue = new StdAttributeValue<>(XACML3.ID_DATATYPE_INTEGER, value);
+        } else if (value instanceof Double) {
+            attributeValue = new StdAttributeValue<>(XACML3.ID_DATATYPE_DOUBLE, value);
+        } else if (value instanceof Boolean) {
+            attributeValue = new StdAttributeValue<>(XACML3.ID_DATATYPE_BOOLEAN, value);
+        } else {
+            throw new IllegalArgumentException("Unsupported value object " + value.getClass());
+        }
+
+        return new StdAttributeAssignment(new IdentifierImpl(category),
+                new IdentifierImpl(id), "", attributeValue);
+    }
+
+    /**
+     * createXacmlObligation.
+     *
+     * @param id String obligation id
+     * @param attributeAssignments Collection of AttributeAssignment objects
+     * @return Obligation object
+     */
+    public static Obligation createXacmlObligation(String id, Collection<AttributeAssignment> attributeAssignments) {
+        return new StdMutableObligation(new IdentifierImpl(id), attributeAssignments);
+    }
+
+    /**
+     * createPolicyIdList.
+     *
+     * @param ids Map of policy Ids
+     * @return {@code Collection<IdReference>} objects
+     * @throws ParseException ParseException
+     */
+    public static Collection<IdReference> createPolicyIdList(Map<String, String> ids) throws ParseException {
+        List<IdReference> policyIds = new ArrayList<>();
+
+        for (Entry<String, String> entrySet : ids.entrySet()) {
+            policyIds.add(new StdIdReference(new IdentifierImpl(entrySet.getKey()),
+                    StdVersion.newInstance(entrySet.getValue())));
+        }
+
+        return policyIds;
+    }
+
+    /**
+     * createXacmlResponse.
+     *
+     * @param code StatusCode
+     * @param decision Decision
+     * @param obligations Collection of Obligation objects
+     * @param policyIds Collection of IdReference objects
+     * @return Response object
+     */
+    public static Response createXacmlResponse(StatusCode code, Decision decision, Collection<Obligation> obligations,
+            Collection<IdReference> policyIds) {
+        StdStatus status = new StdStatus(code);
+
+        StdMutableResult result = new StdMutableResult(decision, status);
+        result.addObligations(obligations);
+        result.addPolicyIdentifiers(policyIds);
+
+        return new StdMutableResponse(result);
+    }
+
+}
