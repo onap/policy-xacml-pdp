f889f2f37fe9c00eff5f06b91dabeaefcbf08827
HOCKLA <ah999m@att.com>
2020-01-02 11:45:22 -0600

    Removed supported policy types from Pdp-Status message
    Issue-ID: POLICY-2230
    Change-Id: I363dc450b6d7aaf13696c50792cbea5a8d643413
    Signed-off-by: HOCKLA <ah999m@att.com>

--- main/src/test/java/org/onap/policy/pdpx/main/XacmlStateTest.java
+++ main/src/test/java/org/onap/policy/pdpx/main/XacmlStateTest.java
@@ -107,9 +107,6 @@ public class XacmlStateTest {
         assertEquals(GROUP, status.getPdpGroup());
         assertEquals(PDP_TYPE, status.getPdpType());
         assertEquals(PdpState.PASSIVE, status.getState());
-        assertEquals("[ToscaPolicyTypeIdentifier(name=nameA, version=typeA), "
-                        + "ToscaPolicyTypeIdentifier(name=nameB, version=typeB)]",
-                        status.getSupportedPolicyTypes().toString());
         assertTrue(status.getPolicies().isEmpty());
 
         // healthy
