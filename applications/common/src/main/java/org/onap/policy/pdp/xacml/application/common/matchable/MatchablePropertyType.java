82a6a9f36d872be0139fc949e139c4cf32d71458
Pamela Dragosh <pdragosh@research.att.com>
2020-03-18 07:50:22 -0400

    Re-factor matchable to reduce complexity
    This solution is much cleaner than what is in StdMatchableTranslator.
    Over 90% code coverage on it - utilizes a callback to retrieve DataType
    and PolicyTypes. Support for missing timestamp TOSCA type. Also can
    do a better job differentiating between a property contained in the policy
    vs a schema.
    
    Changed StdMatchableTranslator to utilize these classes. And removed the
    old spaghetti.
    
    Added some JUnit coverage for ToscaPolicyTranslatorUtils.
    
    NOTE: this patch will not compile.
    
    Issue-ID: POLICY-2242
    Change-Id: I18f898d9e65f6da28e3b27517d40f8d389de18a0
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ applications/common/src/main/java/org/onap/policy/pdp/xacml/application/common/matchable/MatchablePropertyType.java
@@ -0,0 +1,34 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common.matchable;
+
+import com.att.research.xacml.api.Identifier;
+import org.onap.policy.pdp.xacml.application.common.ToscaPolicyConversionException;
+
+public interface MatchablePropertyType<T> {
+
+    public T validate(Object value) throws ToscaPolicyConversionException;
+
+    public Object generate(Object value, Identifier attributeId) throws ToscaPolicyConversionException;
+
+}
---
+++ applications/common/src/main/java/org/onap/policy/pdp/xacml/application/common/matchable/MatchablePropertyTypeMap.java
@@ -0,0 +1,80 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common.matchable;
+
+import com.att.research.xacml.api.Identifier;
+import com.att.research.xacml.std.IdentifierImpl;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import lombok.NonNull;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.AnyOfType;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaEntrySchema;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaProperty;
+import org.onap.policy.pdp.xacml.application.common.ToscaPolicyConversionException;
+import org.onap.policy.pdp.xacml.application.common.ToscaPolicyTranslatorUtils;
+
+public class MatchablePropertyTypeMap extends MatchablePropertyTypeBase<Map<String, MatchablePropertyType<?>>> {
+    MatchableProperty primitiveProperty;
+
+    /**
+     * constructor.
+     *
+     * @param toscaProperty ToscaProperty object
+     */
+    public MatchablePropertyTypeMap(@NonNull ToscaProperty toscaProperty) {
+        super(toscaProperty);
+
+        ToscaEntrySchema schema = toscaProperty.getEntrySchema();
+        this.primitiveProperty = MatchablePolicyType.handlePrimitive(schema.getType(), schema);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Map<String, MatchablePropertyType<?>> validate(Object value) throws ToscaPolicyConversionException {
+        if (value instanceof Map) {
+            return (Map<String, MatchablePropertyType<?>>) value;
+        }
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Object generate(Object value, Identifier attributeId) throws ToscaPolicyConversionException {
+        AnyOfType anyOf = new AnyOfType();
+        for (Entry<String, MatchablePropertyType<?>> entrySet : this.validate(value).entrySet()) {
+            final String id = entrySet.getKey();
+            final Object val = entrySet.getValue();
+            //
+            // For map we use the LHS as part of the attribute id
+            //
+            Identifier newId = new IdentifierImpl(attributeId + ":" + id);
+            //
+            // Build the AnyOfType
+            //
+            anyOf = ToscaPolicyTranslatorUtils.buildAndAppendAllof(anyOf,
+                    primitiveProperty.getType().generate(val, newId));
+        }
+        return anyOf;
+    }
+
+}
