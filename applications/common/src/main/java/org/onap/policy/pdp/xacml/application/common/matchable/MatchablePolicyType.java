7aef8616a492f2377d65d25e40776a0b98e8bc8e
Pamela Dragosh <pdragosh@research.att.com>
2020-03-18 07:50:22 -0400

    Re-factor matchable to reduce complexity
    This solution is much cleaner than what is in StdMatchableTranslator.
    Over 90% code coverage on it - utilizes a callback to retrieve DataType
    and PolicyTypes. Support for missing timestamp TOSCA type. Also can
    do a better job differentiating between a property contained in the policy
    vs a schema.
    
    Issue-ID: POLICY-2242
    Change-Id: I18f898d9e65f6da28e3b27517d40f8d389de18a0
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ applications/common/src/main/java/org/onap/policy/pdp/xacml/application/common/matchable/MatchablePolicyType.java
@@ -0,0 +1,336 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common.matchable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import lombok.Getter;
+import lombok.NonNull;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaDataType;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaEntrySchema;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyIdentifier;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaPolicyType;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+public class MatchablePolicyType {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MatchablePolicyType.class);
+
+    public static final String TOSCA_PRIMITIVE_STRING = "string";
+    public static final String TOSCA_PRIMITIVE_INTEGER = "integer";
+    public static final String TOSCA_PRIMITIVE_FLOAT = "float";
+    public static final String TOSCA_PRIMITIVE_BOOLEAN = "boolean";
+    public static final String TOSCA_PRIMITIVE_TIMESTAMP = "timestamp";
+    public static final String TOSCA_TYPE_LIST = "list";
+    public static final String TOSCA_TYPE_MAP = "map";
+
+    ToscaPolicyIdentifier policyId;
+    Map<String, MatchableProperty> matchables = new HashMap<>();
+
+    public MatchablePolicyType(@NonNull ToscaPolicyType policyType, @NonNull MatchableCallback callback) {
+        this.policyId = new ToscaPolicyIdentifier(policyType.getName(), policyType.getVersion());
+        scanPolicyType(policyType, callback);
+    }
+
+    public MatchableProperty get(@NonNull String property) {
+        return this.matchables.get(property);
+    }
+
+    protected void scanPolicyType(@NonNull ToscaPolicyType inPolicyType, @NonNull MatchableCallback callback) {
+        ToscaPolicyType policyType = inPolicyType;
+        while (policyType != null) {
+            LOGGER.info("Scanning PolicyType {}:{}", policyType.getName(), policyType.getVersion());
+            //
+            // Scan for all the matchable properties
+            //
+            scanProperties(policyType.getProperties(), matchables, callback);
+            //
+            // Does this PolicyType derive from another Policy Type?
+            //
+            if ("tosca.policies.Root".equals(policyType.getDerivedFrom())) {
+                //
+                // No we are finished
+                //
+                LOGGER.info("Found root - done scanning");
+                break;
+            }
+            //
+            // Move to the parent policy and scan it for matchables.
+            //
+            policyType = callback.retrievePolicyType(policyType.getDerivedFrom());
+        }
+    }
+
+    /**
+     * Scans properties for matchables.
+     *
+     * @param properties Map of ToscaProperties to scan
+     * @param matchables Found matchables will be put into this list
+     * @param callback Callback routine for finding Policy Types and Data Types
+     */
+    public static void scanProperties(Map<String, ToscaProperty> properties, Map<String, MatchableProperty> matchables,
+            MatchableCallback callback) {
+        for (Entry<String, ToscaProperty> entrySet : properties.entrySet()) {
+            final String property = entrySet.getKey();
+            final ToscaProperty toscaProperty = entrySet.getValue();
+            //
+            // Most likely case is its a primitive
+            //
+            if (isPrimitive(toscaProperty.getType())) {
+                MatchableProperty primitiveProperty = handlePrimitive(property, toscaProperty);
+                if (primitiveProperty != null) {
+                    matchables.put(property, primitiveProperty);
+                }
+            } else if (TOSCA_TYPE_LIST.equals(toscaProperty.getType())) {
+                MatchableProperty listProperty = handleList(property, toscaProperty, matchables, callback);
+                if (listProperty != null) {
+                    matchables.put(property, listProperty);
+                }
+            } else if (TOSCA_TYPE_MAP.equals(toscaProperty.getType())) {
+                MatchableProperty mapProperty = handleMap(property, toscaProperty, matchables, callback);
+                if (mapProperty != null) {
+                    matchables.put(property, mapProperty);
+                }
+            } else {
+                scanDatatype(toscaProperty, matchables, callback);
+            }
+        }
+    }
+
+    /**
+     * handlePrimitive - handles a primitive type.
+     *
+     * @param property String containing property name
+     * @param toscaProperty ToscaProperty object
+     * @return MatchableProperty object
+     */
+    public static MatchableProperty handlePrimitive(String property, ToscaProperty toscaProperty) {
+        if (!isMatchable(toscaProperty)) {
+            return null;
+        }
+        if (TOSCA_PRIMITIVE_STRING.equals(toscaProperty.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeString(toscaProperty));
+        } else if (TOSCA_PRIMITIVE_INTEGER.equals(toscaProperty.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeInteger(toscaProperty));
+        } else if (TOSCA_PRIMITIVE_BOOLEAN.equals(toscaProperty.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeBoolean(toscaProperty));
+        } else if (TOSCA_PRIMITIVE_FLOAT.equals(toscaProperty.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeFloat(toscaProperty));
+        } else if (TOSCA_PRIMITIVE_TIMESTAMP.equals(toscaProperty.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeTimestamp(toscaProperty));
+        }
+        throw new IllegalArgumentException("Not a primitive " + toscaProperty.getType());
+    }
+
+    /**
+     * handlePrimitive from a schema.
+     *
+     * @param property String containing property name
+     * @param toscaSchema ToscaSchema
+     * @return MatchableProperty object
+     */
+    public static MatchableProperty handlePrimitive(String property, ToscaEntrySchema toscaSchema) {
+        if (TOSCA_PRIMITIVE_STRING.equals(toscaSchema.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeString(toscaSchema));
+        } else if (TOSCA_PRIMITIVE_INTEGER.equals(toscaSchema.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeInteger(toscaSchema));
+        } else if (TOSCA_PRIMITIVE_BOOLEAN.equals(toscaSchema.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeBoolean(toscaSchema));
+        } else if (TOSCA_PRIMITIVE_FLOAT.equals(toscaSchema.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeFloat(toscaSchema));
+        } else if (TOSCA_PRIMITIVE_TIMESTAMP.equals(toscaSchema.getType())) {
+            return new MatchableProperty(property, new MatchablePropertyTypeTimestamp(toscaSchema));
+        }
+        throw new IllegalArgumentException("Not a primitive " + toscaSchema.getType());
+    }
+
+    /**
+     * handleList - iterates a list looking for matchables.
+     *
+     * @param property String containing property name
+     * @param toscaProperty ToscaProperty object
+     * @param matchables list of matchables to add to
+     * @param callback MatchableCallback to retrieve Data Types
+     * @return MatchableProperty object
+     */
+    public static MatchableProperty handleList(@NonNull String property, @NonNull ToscaProperty toscaProperty,
+            Map<String, MatchableProperty> matchables, @NonNull MatchableCallback callback) {
+        if (! TOSCA_TYPE_LIST.equals(toscaProperty.getType())) {
+            throw new IllegalArgumentException("must be a list");
+        }
+        //
+        // Is it a simple or complex list?
+        //
+        if (isPrimitive(toscaProperty.getEntrySchema().getType())) {
+            //
+            // Only if the list is matchable
+            //
+            if (!isMatchable(toscaProperty)) {
+                return null;
+            }
+            //
+            // Simple list of primitives
+            //
+            return new MatchableProperty(property, new MatchablePropertyTypeList(toscaProperty));
+        }
+        //
+        // Scan the datatype for matchables
+        //
+        scanDatatype(toscaProperty.getEntrySchema(), matchables, callback);
+        //
+        // Return nothing - scanning the datatype should find the matchables
+        //
+        return null;
+    }
+
+    /**
+     * handleMap - iterates a map looking for matchables.
+     *
+     * @param property String containing property name
+     * @param toscaProperty ToscaProperty object
+     * @param matchables list of matchables to add to
+     * @param callback MatchableCallback to retrieve Data Types
+     * @return MatchableProperty object
+     */
+    public static MatchableProperty handleMap(@NonNull String property, @NonNull ToscaProperty toscaProperty,
+            Map<String, MatchableProperty> matchables, @NonNull MatchableCallback callback) {
+        if (! TOSCA_TYPE_MAP.equals(toscaProperty.getType())) {
+            throw new IllegalArgumentException("must be a map");
+        }
+        //
+        // Is it a simple or complex map?
+        //
+        if (isPrimitive(toscaProperty.getEntrySchema().getType())) {
+            //
+            // Only if the map is matchable
+            //
+            if (!isMatchable(toscaProperty)) {
+                return null;
+            }
+            //
+            // Simple map of primitives
+            //
+            return new MatchableProperty(property, new MatchablePropertyTypeMap(toscaProperty));
+        }
+        //
+        // Scan the datatype for matchables
+        //
+        scanDatatype(toscaProperty.getEntrySchema(), matchables, callback);
+        //
+        // Return nothing - scanning the datatype should find the matchables
+        //
+        return null;
+    }
+
+    /**
+     * scanDatatype - scans a datatypes schema properties for matchables.
+     *
+     * @param toscaProperty ToscaProperty object
+     * @param matchables list of matchables to add to
+     * @param callback MatchableCallback to retrieve Data Types
+     */
+    public static void scanDatatype(@NonNull ToscaProperty toscaProperty, Map<String, MatchableProperty> matchables,
+            @NonNull MatchableCallback callback) {
+        //
+        // Don't check for matchable property, as that does not make sense to support right now. But we need to
+        // scan the datatype for matchable properties.
+        //
+        LOGGER.info("Retrieving datatype {}", toscaProperty.getType());
+        //
+        // Try to retrieve the datatype
+        //
+        ToscaDataType dataType = callback.retrieveDataType(toscaProperty.getType());
+        if (dataType == null) {
+            LOGGER.error("Failed to retrieve datatype {}", toscaProperty.getType());
+            return;
+        }
+        //
+        // Now scan the properties in the datatype
+        //
+        scanProperties(dataType.getProperties(), matchables, callback);
+    }
+
+    /**
+     * scanDatatypes - scans a datatype schema for matchables.
+     *
+     * @param toscaSchema ToscaEntrySchema
+     * @param matchables list of matchables to add to
+     * @param callback MatchableCallback object for retrieving other data types
+     */
+    public static void scanDatatype(@NonNull ToscaEntrySchema toscaSchema, Map<String, MatchableProperty> matchables,
+            @NonNull MatchableCallback callback) {
+        //
+        // Don't check for matchable property, as that does not make sense to support right now. But we need to
+        // scan the datatype for matchable properties.
+        //
+        LOGGER.info("Retrieving datatype {}", toscaSchema.getType());
+        //
+        // Try to retrieve the datatype
+        //
+        ToscaDataType dataType = callback.retrieveDataType(toscaSchema.getType());
+        if (dataType == null) {
+            LOGGER.error("Failed to retrieve datatype {}", toscaSchema.getType());
+            return;
+        }
+        //
+        // Now scan the properties in the datatype
+        //
+        scanProperties(dataType.getProperties(), matchables, callback);
+    }
+
+    /**
+     * isPrimitive.
+     *
+     * @param type String containing type
+     * @return true if it is a primitive
+     */
+    public static boolean isPrimitive(@NonNull String type) {
+        //@formatter:off
+        return TOSCA_PRIMITIVE_STRING.equalsIgnoreCase(type)
+                || TOSCA_PRIMITIVE_INTEGER.equalsIgnoreCase(type)
+                || TOSCA_PRIMITIVE_FLOAT.equalsIgnoreCase(type)
+                || TOSCA_PRIMITIVE_BOOLEAN.equalsIgnoreCase(type)
+                || TOSCA_PRIMITIVE_TIMESTAMP.equalsIgnoreCase(type);
+        //@formatter:on
+    }
+
+    /**
+     * isMatchable - scans metadata for matchable field set to true.
+     *
+     * @param property ToscaProperty object
+     * @return true if matchable
+     */
+    public static boolean isMatchable(@NonNull ToscaProperty property) {
+        if (property.getMetadata() == null) {
+            return false;
+        }
+        String isMatchable = property.getMetadata().get("matchable");
+        if (isMatchable == null) {
+            return false;
+        }
+        return "true".equalsIgnoreCase(isMatchable);
+    }
+}
---
+++ applications/common/src/main/java/org/onap/policy/pdp/xacml/application/common/matchable/MatchablePropertyTypeFloat.java
@@ -0,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pdp.xacml.application.common.matchable;
+
+import com.att.research.xacml.api.Identifier;
+import com.att.research.xacml.api.XACML3;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.AllOfType;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.MatchType;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.TargetType;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaEntrySchema;
+import org.onap.policy.models.tosca.authorative.concepts.ToscaProperty;
+import org.onap.policy.pdp.xacml.application.common.ToscaPolicyConversionException;
+import org.onap.policy.pdp.xacml.application.common.ToscaPolicyTranslatorUtils;
+
+public class MatchablePropertyTypeFloat extends MatchablePropertyTypeBase<Float> {
+
+    public MatchablePropertyTypeFloat(ToscaProperty inProperty) {
+        super(inProperty);
+    }
+
+    public MatchablePropertyTypeFloat(ToscaEntrySchema toscaSchema) {
+        super(toscaSchema);
+    }
+
+    @Override
+    public Float validate(Object value) throws ToscaPolicyConversionException {
+        //
+        // Most likely it isn't because Gson does not recognize floats
+        //
+        if (value instanceof Float) {
+            return (Float) value;
+        }
+        try {
+            return Float.parseFloat(value.toString());
+        } catch (NumberFormatException e) {
+            throw new ToscaPolicyConversionException("Bad float value" + value.toString(), e);
+        }
+    }
+
+    @Override
+    public void generate(TargetType target, Object value, Identifier attributeId)
+            throws ToscaPolicyConversionException {
+        MatchType match = ToscaPolicyTranslatorUtils.buildMatchTypeDesignator(
+                XACML3.ID_FUNCTION_INTEGER_EQUAL,
+                validate(value).toString(),
+                XACML3.ID_DATATYPE_INTEGER,
+                attributeId,
+                XACML3.ID_ATTRIBUTE_CATEGORY_RESOURCE);
+        AllOfType allOf = new AllOfType();
+        allOf.getMatch().add(match);
+        //
+        // Add to the first AllOf - in the future we will use other metadata and/or
+        // constraints to dictate how the TargetType should be setup.
+        //
+        target.getAnyOf().get(0).getAllOf().add(allOf);
+    }
+
+}
