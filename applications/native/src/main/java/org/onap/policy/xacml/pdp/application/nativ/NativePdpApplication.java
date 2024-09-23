f5131dc5e0e131fda0fbb07dcb62e0aeac78988a
ajinkya-patil <ajinkya-patil@t-systems.com>
2024-09-23 15:25:46 +0530

    Added tosca xacml policy type in Native Application
    Made changes as per suggested in sonar qube
    
    Issue-ID: POLICY-5128
    Change-Id: I2ff717cef3478406783dda6051c8fbec65e8fcef
    Signed-off-by: ajinkya-patil <ajinkya-patil@t-systems.com>

--- applications/native/src/main/java/org/onap/policy/xacml/pdp/application/nativ/NativePdpApplication.java
+++ applications/native/src/main/java/org/onap/policy/xacml/pdp/application/nativ/NativePdpApplication.java
@@ -4,6 +4,7 @@
  * ================================================================================
  * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
  * Modifications Copyright (C) 2021 Nordix Foundation.
+ * Modifications Copyright (C) 2024 Deutsche Telekom AG.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
